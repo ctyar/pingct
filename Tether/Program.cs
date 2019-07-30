c436c8011206a2b3d350077b8b652ecb408e6bde
Shahriar Gholami <shrgholami@gmail.com>
2019-07-30 11:49:10 +0430

    Add nlog to catch the crash

--- Tether/Program.cs
+++ Tether/Program.cs
@@ -1,4 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Tether
 {
@@ -6,9 +14,47 @@ namespace Tether
     {
         private static async Task Main(string[] args)
         {
-            var testManager = new TestManager();
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                logger.Info("Application started.");
 
-            await testManager.Scan();
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+
+                var servicesProvider = BuildDi(config);
+                using (servicesProvider as IDisposable)
+                {
+                    var testManager = servicesProvider.GetRequiredService<TestManager>();
+
+                    await testManager.Scan();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                logger.Info("Exiting application.");
+                LogManager.Shutdown();
+            }
+        }
+
+        private static IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+                .AddTransient<TestManager>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                })
+                .BuildServiceProvider();
         }
     }
 }
