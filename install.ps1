671e04f78e34949b6fa45d8cb16330b9ed8ee960
ctyar <ctyar@outlook.com>
2023-01-28 11:01:29 +0000

    Update NuGet packages

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem -Path '.\artifacts' | Remove-Item -Force -Recurse
 
-dotnet pack --configuration Release -o artifacts
+dotnet pack src\pingct\pingct.csproj --configuration Release -o artifacts
 
 $version = (get-item .\artifacts\*.nupkg).Name -replace "pingct.","" -replace ".nupkg",""
 
